f0038400ec32737b57ed9307a1a82d4b16b9ccb6
Công Quyền <congquyenhuynh112@gmail.com>
2024-11-06 12:45:55 +0700

    add docker

---
+++ Persistence/Migrations/20241029175035_Fix_BookingStatus.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fix_BookingStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+
+            migrationBuilder.DropColumn(
+                name: "SeatNumber",
+                table: "Tickets");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "SeatId",
+                table: "Tickets",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_SeatId",
+                table: "Tickets",
+                column: "SeatId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tickets_Seats_SeatId",
+                table: "Tickets",
+                column: "SeatId",
+                principalTable: "Seats",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tickets_Seats_SeatId",
+                table: "Tickets");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tickets_SeatId",
+                table: "Tickets");
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("0c9e0c78-e0ec-46b3-b510-d7754d5be8dd"));
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "Id",
+                keyValue: new Guid("6aa2d54c-8b2c-4e2a-9ce1-7a0c1b1d2555"));
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("0c9e0c78-e0ec-46b3-b510-d7754d5be8dd"));
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("6aa2d54c-8b2c-4e2a-9ce1-7a0c1b1d2555"));
+
+            migrationBuilder.DropColumn(
+                name: "SeatId",
+                table: "Tickets");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SeatNumber",
+                table: "Tickets",
+                type: "character varying(10)",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { new Guid("7d5c108d-53b4-46d4-93fe-e5fa44aadb0a"), "1", "Admin", "ADMIN" },
+                    { new Guid("83fab4fe-27ec-4fdc-9a82-b90dda084eed"), "5", "User", "USER" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                column: "Id",
+                values: new object[]
+                {
+                    new Guid("7d5c108d-53b4-46d4-93fe-e5fa44aadb0a"),
+                    new Guid("83fab4fe-27ec-4fdc-9a82-b90dda084eed")
+                });
+        }
+    }
+}
